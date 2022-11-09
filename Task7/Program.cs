6df1854d36dca2dfb4bb7b743207ea377dc98337
Kate <cvetok_spb@bk.ru>
2022-11-09 14:23:06 +0300

    Task7

---
+++ Task7/Program.cs
@@ -0,0 +1,52 @@
+﻿// Написать программу, которая обменивает элементы первой строки и последней строки
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+
+        }
+
+    }
+
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],4}, "); // 4 добавили чтобы код в консоли выводлся красиво
+            else Console.Write($"{matrix[i, j],4}"); // 4 добавили чтобы код в консоли выводлся красиво
+        }
+        Console.WriteLine("]");
+    }
+}
+
+void PrintRowsColumsIndex(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(1); i++)
+    {
+        int temp = matrix[0, i];
+        matrix[0, i] = matrix[matrix.GetLength(0) - 1, i];
+        matrix[matrix.GetLength(0) - 1, i] = temp;
+    }
+
+}
+
+int[,] array2D = CreateMatrixRndInt(3, 4, 1, 10);
+PrintMatrix(array2D);
+Console.WriteLine("");
+PrintRowsColumsIndex(array2D);
+PrintMatrix(array2D);
